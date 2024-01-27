ba106fb8bd0c70da24f8d3c9907e0a53634d2f69
Виктория <viksyaavito@gmail.com>
2024-01-27 22:03:44 +0300

    invalid response test

--- src/test/java/ru/netology/service/MainTest.java
+++ src/test/java/ru/netology/service/MainTest.java
@@ -16,7 +16,7 @@ class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello"))
+                .body("data", equalTo("Hello1"))
         ;
     }
 }
