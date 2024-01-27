77f694dc00ab1d1180cdcac2d1b3ca13182659b6
Виктория <viksyaavito@gmail.com>
2024-01-27 22:02:17 +0300

    postman echo test

---
+++ src/test/java/ru/netology/service/MainTest.java
@@ -0,0 +1,22 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello"))
+        ;
+    }
+}
