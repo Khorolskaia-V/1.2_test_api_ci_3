9bfeb8bf891cc0f9740d627dd701e6dc61606047
Виктория <viksyaavito@gmail.com>
2024-01-27 22:04:21 +0300

    restore valid test

--- src/test/java/ru/netology/service/MainTest.java
+++ src/test/java/ru/netology/service/MainTest.java
@@ -16,7 +16,7 @@ class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello1"))
+                .body("data", equalTo("Hello"))
         ;
     }
 }
